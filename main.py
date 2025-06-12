51b05bc96f03283d8869131f9567fa77bf1e61f6
Adwaith <adwaithhs@gmail.com>
2025-06-12 21:52:38 +0530

    complete this

--- main.py
+++ main.py
@@ -6,3 +6,17 @@ for i in range(10):
     next = a + b
     a = b
     b = next
+
+
+def get_nth_fibnocci(n):
+    if n <= 0:
+        return 0
+
+    nth_fibanocci = 1 # find the nth nth_fibanocci HERE
+    return nth_fibanocci
+
+
+
+print('The fifth fibanocci number is', get_nth_fibnocci(5))
+
+
